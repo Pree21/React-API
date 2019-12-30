64ebc8c562f7ed70ad3d487198f5468b87c3f5e4
Preeti Aggarwal <Pree21@users.noreply.github.com>
2019-12-30 13:25:14 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
+import Modal from 'react-responsive-modal';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isFavourite: false,
+      open: false,
     }
   }
-  handleClick=()=> {
+  handleClick = () => {
     this.setState(prevState => ({
       isFavourite: !prevState.isFavourite,
     }))
   }
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
   render() {
-    const {item}=this.props;
+    const { item } = this.props;
     const favselected = (this.state.isFavourite) ? "fa fa-star" : "fa fa-star-o"
     return (
       <div className="box">
-        <div className="star">
-          <i className={favselected} onClick={this.handleClick}></i>
+        <div className="box-in" onClick={this.onOpenModal}>
+          <div className="star">
+            <i className={favselected} onClick={this.handleClick}></i>
+          </div>
+          <div className="img-box">
+            <img src={item.image_url} />
+          </div>
+          <div className="item-desc">
+            <h4>{item.name}</h4>
+            <p>{item.description}</p>
+          </div>
         </div>
-        <div className="img-box">
-          <img src={item.image_url} />
-        </div>
-        <div className="item-desc">
-          <h4>{item.name}</h4>
-          <p>{item.description}</p>
-        </div>
+
+
+        <Modal open={this.state.open} onClose={this.onCloseModal} center>
+          <div className="box-in modal-content">
+            <div className="img-box">
+              <img src={item.image_url} />
+            </div>
+            <div className="item-desc">
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        </Modal>
+
       </div>
     );
   }
